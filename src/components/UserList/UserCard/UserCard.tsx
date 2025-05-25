4362828c05f0125a0d878d6bbb2b257f41b56383
JavadAg <j.aghebati93@gmail.com>
2025-05-26 02:45:20 +0330

    fix: correct flag image source path in UserCard component

--- src/components/UserList/UserCard/UserCard.tsx
+++ src/components/UserList/UserCard/UserCard.tsx
@@ -64,7 +64,7 @@ const UserCard: React.FC<UserCardProps> = memo(
 
         <Image
           className="user-card__flag"
-          src={`/flags/${user.nationality.toLowerCase()}.png`}
+          src={`/flags/${user.nationality}.png`}
           alt={`Flag of ${user.nationality}`}
           width={24}
           height={16}
